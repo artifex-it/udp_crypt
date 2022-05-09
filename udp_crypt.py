8f4518a9fd28af1de1437e71bdf8a204364efe0b
Paolo Betti <paolo@artifex.it>
2022-05-09 18:21:39 +0200

    First push

---
+++ udp_crypt.py
@@ -0,0 +1,229 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from asyncio import get_event_loop
+from socket import AF_INET, SOCK_DGRAM, socket, timeout
+from sys import argv
+
+SERVER_ADDR = "localhost"
+ADDR_TO_CONNECT = "localhost" # "localhost" or "rp1.cdoctor.it"
+PORT = 65530
+
+CARBONARO_ENC_TABLE = dict(zip("ABCDEFGHIJKLMNOPQRSTUVWXYZ", "OPGTIVCHEJKRNMABQLZDUFWXYS"))
+CARBONARO_DEC_TABLE = {v: k for k, v in CARBONARO_ENC_TABLE.items()}
+
+class CryptedMessage:
+    '''
+    The encryption and decryption management.
+
+    Message C struc:
+    #define DIM_WORM 16
+    #define DIM_MESSAGE 256
+    struct message {
+        uint8_t algoritm; // cesare_encrypt=0, ROT13=1, atbash=2, carbonaro=3, affine_encrypt=4, vigenere_encrypt=5, baconian_encrypt=6
+        uint8_t a;
+        uint8_t b;
+        char worm[DIM_WORM+1];
+        char mess[DIM_MESSAGE+1];
+    }
+    C struct message size: 1 + 1 + 1 + 17 (16+1) + 257 (256+1) = 277
+    '''
+    algoritm = 0
+    a = 0
+    b = 0
+    worm = b"\0"*17
+    mess = b"\0"*257 # Message encrypted
+
+    def __init__(self, message_bytes=None):
+        '''
+        '''
+        if message_bytes:
+            self.algoritm = int.from_bytes(message_bytes[0:1], "big")
+            self.a = int.from_bytes(message_bytes[1:2], "big")
+            self.b = int.from_bytes(message_bytes[2:3], "big")
+            self.worm = message_bytes[3:20]
+            self.mess = message_bytes[20:277]
+
+    def encrypt(self, msg:str):
+        '''
+        '''
+        crypted_msg = b""
+
+        # cesare_encrypt
+        if self.algoritm == 0:
+            for c in msg:
+                if c.islower():
+                    crypted_ord = ord('a') + (ord(c) - ord('a') + 3) % 26
+                elif c.isupper():
+                    crypted_ord = ord('A') + (ord(c) - ord('A') + 3) % 26
+                else:
+                    crypted_ord = 46 # '.'
+                crypted_msg += chr(crypted_ord).encode("utf-8")
+        # ROT13
+        elif self.algoritm == 1:
+            for c in msg:
+                if c.islower():
+                    crypted_ord = ord('a') + (ord(c) - ord('a') + 13) % 26
+                elif c.isupper():
+                    crypted_ord = ord('A') + (ord(c) - ord('A') + 13) % 26
+                else:
+                    crypted_ord = 46 # '.'
+                crypted_msg += chr(crypted_ord).encode("utf-8")
+        # adbash
+        elif self.algoritm == 2:
+            for c in msg:
+                if c.islower():
+                    crypted_ord = ord('a') + (ord('z') - ord(c)) % 26
+                elif c.isupper():
+                    crypted_ord = ord('A') + (ord('Z') - ord(c)) % 26
+                else:
+                    crypted_ord = 46 # '.'
+                crypted_msg += chr(crypted_ord).encode("utf-8")
+        # carbonaro
+        elif self.algoritm == 3:
+            for c in msg:
+                if c.islower():
+                    crypted_c = CARBONARO_ENC_TABLE[c.upper()].lower()
+                elif c.isupper():
+                    crypted_c = CARBONARO_ENC_TABLE[c]
+                else:
+                    crypted_c = '.'
+                crypted_msg += crypted_c.encode("utf-8")
+
+
+        self.mess = crypted_msg + b"\0"*(257-len(crypted_msg))
+
+    def decrypt(self)->str:
+        '''
+        '''
+        decrypted_msg = ""
+
+        # cesare_encrypt
+        if self.algoritm == 0:
+            for c_ord in self.mess:
+                if ord('a') <= c_ord <= ord('z'):
+                    decrypted_ord = ord('a') + (c_ord - ord('a') - 3) % 26
+                elif ord('A') <= c_ord <= ord('Z'):
+                    decrypted_ord = ord('A') + (c_ord - ord('A') - 3) % 26
+                elif c_ord != 0:
+                    decrypted_ord = 46 # '.'
+                if c_ord > 0:
+                    decrypted_msg += chr(decrypted_ord)
+        # ROT13
+        elif self.algoritm == 1:
+            for c_ord in self.mess:
+                if ord('a') <= c_ord <= ord('z'):
+                    decrypted_ord = ord('a') + (c_ord - ord('a') - 13) % 26
+                elif ord('A') <= c_ord <= ord('Z'):
+                    decrypted_ord = ord('A') + (c_ord - ord('A') - 13) % 26
+                elif c_ord != 0:
+                    decrypted_ord = 46 # '.'
+                if c_ord > 0:
+                    decrypted_msg += chr(decrypted_ord)
+        # adbash
+        if self.algoritm == 2:
+            for c_ord in self.mess:
+                if ord('a') <= c_ord <= ord('z'):
+                    decrypted_ord = ord('a') + (ord('z') - c_ord) % 26
+                elif ord('A') <= c_ord <= ord('Z'):
+                    decrypted_ord = ord('A') + (ord('Z') - c_ord) % 26
+                elif c_ord != 0:
+                    decrypted_ord = 46 # '.'
+                if c_ord > 0:
+                    decrypted_msg += chr(decrypted_ord)
+        # carbonaro
+        if self.algoritm == 3:
+            for c_ord in self.mess:
+                if ord('a') <= c_ord <= ord('z'):
+                    decrypted_c = CARBONARO_DEC_TABLE[chr(c_ord).upper()].lower()
+                elif ord('A') <= c_ord <= ord('Z'):
+                    decrypted_c = CARBONARO_DEC_TABLE[chr(c_ord)]
+                elif c_ord != 0:
+                    decrypted_c = '.'
+                if c_ord > 0:
+                    decrypted_msg += decrypted_c
+
+        return decrypted_msg
+
+    def get_bytes(self)->str:
+        '''
+        '''
+        message_bytes = self.algoritm.to_bytes(1, "big")
+        message_bytes += self.a.to_bytes(1, "big")
+        message_bytes += self.b.to_bytes(1, "big")
+        message_bytes += self.worm
+        message_bytes += self.mess
+        return message_bytes
+
+
+class ServerProtocol():
+    '''
+    '''
+    transport = None
+
+    def connection_made(self, transport):
+        '''
+        '''
+        self.transport = transport
+
+    def datagram_received(self, message_bytes, addr):
+        '''
+        '''
+        message = CryptedMessage(message_bytes)
+        decrypted_message = message.decrypt()
+        print(f"Received {decrypted_message} from {addr}")
+        response = CryptedMessage()
+        response.algoritm = message.algoritm
+        response.encrypt(decrypted_message+" ok")
+        self.transport.sendto(response.get_bytes(), addr)
+
+
+if __name__ == '__main__':
+    # Server
+    if "-s" in argv:
+        loop = get_event_loop()
+        print("Starting UDP server")
+
+        listen = loop.create_datagram_endpoint(ServerProtocol, (SERVER_ADDR, PORT))
+        transport, protocol = loop.run_until_complete(listen)
+
+        try:
+            loop.run_forever()
+        except KeyboardInterrupt:
+            pass
+
+        transport.close()
+        loop.close()
+
+    # Client
+    else:
+        algoritms = {
+            0: "cesare_encrypt",
+            1: "ROT13",
+            2: "adbash",
+            3: "carbonaro",
+        }
+
+        addr = (ADDR_TO_CONNECT, PORT)
+
+        msg_sended = "Ciao Ragazzo"
+        message = CryptedMessage()
+
+        for i, a in algoritms.items():
+            client = socket(AF_INET, SOCK_DGRAM)
+            client.settimeout(1.0)
+
+            message.algoritm = i
+            message.encrypt(msg_sended)
+
+            message_bytes = message.get_bytes()
+
+            client.sendto(message_bytes, addr)
+            try:
+                response_bytes, server = client.recvfrom(277)
+                response_message = CryptedMessage(response_bytes)
+                print(f"{a}: {msg_sended} -> {response_message.decrypt()}")
+            except timeout:
+                print(f"{a}: REQUEST TIMED OUT")
+                print(message_bytes)
+            client.close()
